eba138b1bceb0b2dd59d89e61088ec453546d4a9
c mulligan <gateless@protonmail.com>
2018-07-30 17:45:39 -0400

    make working copy of engine.js; add checkCollision()

--- js/app.js
+++ js/app.js
@@ -2,10 +2,11 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x;
+    this.y;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +15,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+  
 };
 
 // Draw the enemy on the screen, required method for game
